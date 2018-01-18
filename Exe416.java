8c5a919b0b2d20a768b9629e4e0128d9797aebe4
silviagomez <silvgomezr@gmail.com>
2018-01-18 15:29:42 -0500

    Add files via upload

---
+++ Exe416.java
@@ -0,0 +1,63 @@
+public class Exe416 {
+    public static void main(String[] args){
+        int randNum = (int)(Math.random()*27);
+        String randLetter ="";
+        switch(randNum){
+            case 1:
+                randLetter ="A"; break;
+            case 2:
+                randLetter ="B"; break;
+            case 3:
+                randLetter ="C"; break;
+            case 4:
+                randLetter ="D"; break;
+            case 5:
+                randLetter ="E"; break;
+            case 6:
+                randLetter ="F"; break;
+            case 7:
+                randLetter ="G"; break;
+            case 8:
+                randLetter ="H"; break;
+            case 9:
+                randLetter ="I"; break;
+            case 10:
+                randLetter ="J"; break;
+            case 11:
+                randLetter ="K"; break;
+            case 12:
+                randLetter ="L"; break;
+            case 13:
+                randLetter ="M"; break;
+            case 14:
+                randLetter ="N"; break;
+            case 15:
+                randLetter ="O"; break;
+            case 16:
+                randLetter ="P"; break;
+            case 17:
+                randLetter ="Q"; break;
+            case 18:
+                randLetter ="R"; break;
+            case 19:
+                randLetter ="S"; break;
+            case 20:
+                randLetter ="T"; break;
+            case 21:
+                randLetter ="U"; break;
+            case 22:
+                randLetter ="V"; break;
+            case 23:
+                randLetter ="W"; break;
+            case 24:
+                randLetter ="X"; break;
+            case 25:
+                randLetter ="Y"; break;
+            case 26:
+                randLetter ="Z"; break;
+
+        }
+        System.out.println("The random number created is: "+randNum);
+        System.out.println("Hence, the random letter is: "+randLetter);
+    }
+}
